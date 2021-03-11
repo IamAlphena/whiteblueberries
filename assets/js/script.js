9fe64019d5ca6851bfcb38911d01cb0022377b4b
bhfreeman <brett.h.freeman@gmail.com>
2021-03-11 11:01:09 -0700

    adding merged files

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,36 +9,17 @@ function getMeal(ingredient) {
     fetch(mealByIngredientURL)
     // setting the response to json
     .then((data) => data.json())
-    // function to push the id into the idArray
     .then(function (data) {
         console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            const item = data[i];
+        for (let i = 0; i < data.meals.length; i++) {
+            var item = data.meals[i];
             var mealName = item.strMeal;
             var mealImg = item.strMealThumb;
             console.log(mealName);
             console.log(mealImg);
         }
-        // console.log(idArray);
-        // console.log(idArray[0])
     });
 }
 
 
 getMeal("chicken");
-var blueberies = document.querySelector
-
-// declare a function
-function makeCookies (flavor, servedWarm) {
-    console.log(servedWarm);
-
-    if (servedWarm){
-        console.log(`here is a warm ${flavor} cookies`);
-    } else{
-        console.log(`here is a cold ${flavor} cookies`);
-    }
-    
-}
-// call a function
-makeCookies("sugar", true);
-makeCookies("chocolate chip", false);
